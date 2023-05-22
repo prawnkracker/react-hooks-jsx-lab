4c936fa6baa37d824298892796cf5cb9d952d5c1
Dennis Demir <dennisjdemir@gmail.com>
2023-05-22 16:54:59 -0400

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
-}
+  return (
+  <div id='about'>
+    <h2>About Me</h2>
+    <p>My real first name and middle name is Dennis James and that's where DeeJay derives from!</p>
+    <img src={ image } alt='I made this' />
+  </div>
+)};
 
 export default About;
